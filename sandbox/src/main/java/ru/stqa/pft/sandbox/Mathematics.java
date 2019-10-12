4fbea3e2508b67b946422d1bfe073c51c49e81fb
sergienko <cergienko@inbox.ru>
2019-10-12 20:07:17 +0300

    new class Mathematics

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Mathematics.java
@@ -0,0 +1,37 @@
+//Пакет
+package ru.stqa.pft.sandbox;
+
+//Класс
+public class Mathematics {
+
+  //	Метод или функция.
+  //	Всегда после объявления стоят () для параметров
+  public static void main(String[] args) {
+
+//    sout - сокращенное сочетание клавишь System.out.println
+
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
+
+//  По умолчанию используются целые числа. Для дробных нужно указать 1.0
+    System.out.println(1 / 2);
+    System.out.println(1.0 / 2);
+    System.out.println(1 / 2.0);
+    System.out.println(2.0 / 2);
+
+    System.out.println("2" + "2");
+    System.out.println("2" + 3);
+    System.out.println(3 + "2");
+
+    System.out.println(2 + 2 * 2);
+    System.out.println((2 + 2) *2);
+
+//    Конкотонация. прирете выполнения операций.
+//    тк скобок нет то преобразует в строку
+    System.out.println("2+2=" + 2 + 2);
+    System.out.println(("2+2=" + (2 + 2)));
+  }
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,7 @@
-//Пакет
 package ru.stqa.pft.sandbox;
 
-//Класс
 public class MyFirstProgram {
 
-  //	Метод или функция.
-  //	Всегда после объявления стоят () для параметров
   public static void main(String[] args) {
     System.out.println("Hellow world");
   }
