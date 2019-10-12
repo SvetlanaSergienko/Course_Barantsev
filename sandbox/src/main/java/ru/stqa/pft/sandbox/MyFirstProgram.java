900def194c848af405d30ee8f06f23ee8f69cc9d
sergienko <cergienko@inbox.ru>
2019-10-12 19:33:19 +0300

    Добавлены комментарии о пакете, классе, методе в код

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,13 @@
+//Пакет
 package ru.stqa.pft.sandbox;
 
+//Класс
 public class MyFirstProgram {
 
-	public static void main(String[] args){
-		System.out.println("Hellow world");
+  //	Метод или функция.
+  //	Всегда после объявления стоят () для параметров
+  public static void main(String[] args) {
+    System.out.println("Hellow world");
+  }
 
-
-	}
 }
