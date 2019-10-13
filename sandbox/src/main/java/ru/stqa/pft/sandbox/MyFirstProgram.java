7023d51c9d4d1234759a347889388de0f6d9f3ae
sergienko <cergienko@inbox.ru>
2019-10-13 17:02:17 +0300

    Функции переделаны в методы
    Функции Square и Rectangle переделаны в методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,25 +9,19 @@ public class MyFirstProgram {
 
 //  Создание объекта тип s который берет переменные из класса Square. Значение подставляет конструктором.
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+//    s.area() - ссылка на метод
+//    area(s) - ссылка на функцию (в этом же классе)
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
 
 //  Создание объекта тип r который берет переменные из класса Rectangle. Значение подставляет конструктором.
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со стронами " + r.a + " и " + r.b + " = " + area(r));
+//    r.area() - ссылка на метод
+    System.out.println("Площадь прямоугольника со стронами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
   public static void hello(String somebody){
     System.out.println("Hello, " + somebody + "!");
-  }
-
-//  функция формулы площади квадрата. Ссылка на отдельный класс с переменной a
-  public static double area(Square s){
-    return s.l * s.l;
-  }
-//  функция формулы площади прямоугольника. Ссылка на отдельный класс с переменной a b
-  public static double area (Rectangle r){
-    return r.a * r.b;
   }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -1,15 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 /**
- * Класс Прямоугольника с конструктором  public Rectangle
+ * Класс Прямоугольника с конструктором  public Rectangle и методом area
  */
 
 public class Rectangle {
   public double a;
   public double b;
 
+//  Конструктор
   public Rectangle (double a, double b){
     this.a = a;
     this.b = b;
+  }
+
+  //  метод
+  public double area (){
+    return this.a * this.b;
   }
 }
