e71cacd1a87d13a7993315d18d8a0a22c0ea9eb7
sergienko <cergienko@inbox.ru>
2019-10-13 14:25:57 +0300

    Добавила комментарий об использовании  класса в другом классе

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,10 +7,12 @@ public class MyFirstProgram {
     hello("user");
     hello("Alexsei");
 
+//  Создание объекта тип s который берет переменные из класса Square
     Square s = new Square();
     s.l = 5;
     System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
 
+//  Создание объекта тип r который берет переменные из класса Rectangle
     Rectangle r = new Rectangle();
     r.a = 4;
     r.b = 6;
@@ -22,11 +24,11 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-//  функция формулы площади квадрата
+//  функция формулы площади квадрата. Ссылка на отдельный класс с переменной a
   public static double area(Square s){
     return s.l * s.l;
   }
-//  функция формулы площади прямоугольника
+//  функция формулы площади прямоугольника. Ссылка на отдельный класс с переменной a b
   public static double area (Rectangle r){
     return r.a * r.b;
   }
