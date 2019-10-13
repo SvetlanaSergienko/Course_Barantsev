8ee33e854e651d2841d2452226830e9b9d323f0d
sergienko <cergienko@inbox.ru>
2019-10-13 12:40:30 +0300

    new class Variables - переменные

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Variables.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+//Переменные
+public class Variables {
+
+  public static void main(String[] args) {
+    String somebody = "world!";
+    System.out.println("Hello, " + somebody);
+
+//    Переменные
+//    int - целые числа
+//    double - числа с плавающией точкой
+//    String - строковые переменные
+
+    double l = 8.0;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + (l * l));
+    System.out.println("Площадь квадрата со стороной " + l + "=" + s);
+
+  }
+
+}
